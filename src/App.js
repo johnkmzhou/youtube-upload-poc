fa00a923d3a98c675956198886d0d9749777ec3e
John Zhou <john.km.zhou@gmail.com>
2020-05-28 17:10:28 -1000

    Add ability to upload videos to youtube.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import { FirebaseAuth } from 'react-firebaseui';
+import firebase from 'firebase/app';
+import 'firebase/auth'
+import { MediaUploader } from './cors_upload';
+
+const config = {
+  apiKey: "AIzaSyA9wVUprRRPNyaWX8L4ryaiu32Hs2g2ByI",
+  authDomain: "upload-poc-bdf60.firebaseapp.com",
+}
+
+firebase.initializeApp(config);
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [signedIn, setSignedIn] = useState(false);
+  const [accessToken, setAccessToken] = useState();
+  const [percentageComplete, setPercentageComplete] = useState(0);
+  const [uploadResponse, setUploadResponse] = useState();
+  const fileRef = useRef();
+
+  const uiConfig = {
+    signInFlow: 'redirect',
+    signInOptions: [{
+      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      scopes: [
+        'https://www.googleapis.com/auth/youtube.upload'
+      ]
+    }
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: authResult => {
+        console.log(authResult);
+        setAccessToken(authResult.credential.accessToken);
+        setSignedIn(true);
+      }
+    }
+  };
+
+  const onSubmit = async event => {
+    try {
+      event.preventDefault();
+      const file = fileRef.current.files[0];
+
+      const metadata = {
+        snippet: {
+          title: title,
+        },
+        status: {
+          privacyStatus: 'unlisted'
+        }
+      }
+
+      const uploader = new MediaUploader({
+        baseUrl: 'https://www.googleapis.com/upload/youtube/v3/videos',
+        file: file,
+        token: accessToken,
+        metadata: metadata,
+        params: {
+          part: Object.keys(metadata).join(',')
+        },
+        onError: (data) => {
+          setUploadResponse(data);
+        },
+        onProgress: (data) => {
+          var bytesUploaded = data.loaded;
+          var totalBytes = data.total;
+          setPercentageComplete((bytesUploaded * 100) / totalBytes);
+        },
+        onComplete: (data) => {
+          setUploadResponse(data);
+        }
+      })
+
+      uploader.upload();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {signedIn ? <>
+        <h1>My App</h1>
+        <p>Welcome! You are now signed-in!</p>
+        <form onSubmit={onSubmit}>
+          <input name="title" type="text" placeholder="Title" value={title} onChange={event => setTitle(event.target.value)} />
+          <input name="file" type="file" accept="video/mp4" ref={fileRef} />
+          <button type="submit">Submit</button>
+        </form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {percentageComplete}{"% uploaded"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {uploadResponse}
+      </> : <>
+          <h1>Youtube Upload Proof of Concept</h1>
+          <p>Please sign-in:</p>
+          <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </>}
     </div>
   );
 }
